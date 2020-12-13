a6ad521f4eae814094c2e4d456af3c6e2630315d
Lily Riopelle <lilykriopelle@gmail.com>
2020-12-12 20:22:58 -0500

    Remove upvotes/downvotes.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Comment({ comment }) {
-  const { id, text, upvotes, downvotes } = comment
+  const { id, text } = comment
   return (
     <div key={id} className="comment-container">
       <span>{text}</span>
