4c7ac5fe852c4d8abff0c218940785c29151946b
Lily Riopelle <lilykriopelle@gmail.com>
2020-12-29 15:18:53 -0500

    Remove logic from Comments.js.

--- src/features/comments/Comments.js
+++ src/features/comments/Comments.js
@@ -8,21 +8,12 @@ import CommentForm from '../../components/CommentForm'
 const Comments = () => {
   const dispatch = useDispatch();
   const article = useSelector(selectCurrentArticle);
-  const comments = useSelector(selectComments)
-  const commentsForArticleId = article ? comments[article.id] : [];
-  const commentsAreLoading = useSelector(isLoadingComments);
 
-  useEffect(() => {
-    if (article) dispatch(loadCommentsForArticleId(article.id));
-  }, [dispatch, article]);
-
-  if (commentsAreLoading) return <div>Loading Comments</div>;
   if (!article) return <div>No article selected.</div>;
 
   return (
     <div className='comments-container'>
       <CommentForm articleId={article.id}/>
-      <CommentList comments={commentsForArticleId}/>
     </div>
   );
 };
