66464b0279d5e0e5299a7f48e348888c5090d11d
John Pham <johnphammail@gmail.com>
2021-01-05 18:02:28 -0800

    Add styles

--- src/components/ArticleListItem.js
+++ src/components/ArticleListItem.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export default function ArticleListItem({ article }) {
   return (
-    <div key={article.id} className='article-container'>
-      <h3 className='article-title'>{article.title}</h3>
-      <div className='article-preview'>{article.preview}</div>
-    </div>
+    <button key={article.id} className='article-container'>
+      <img src={article.image} alt='' className='article-image' />
+      <div className='article-content-container'>
+        <h3 className='article-title'>{article.title}</h3>
+        <p className='article-preview'>{article.preview}</p>
+      </div>
+    </button>
   );
 }
--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,23 +1,42 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { postCommentForArticleId, createCommentIsPending } from '../features/comments/commentsSlice';
+import {
+  postCommentForArticleId,
+  createCommentIsPending,
+} from '../features/comments/commentsSlice';
 
-export default function CommentForm({articleId}) {
+export default function CommentForm({ articleId }) {
   const dispatch = useDispatch();
   const [comment, setComment] = useState('');
   const createPending = useSelector(createCommentIsPending);
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    dispatch(postCommentForArticleId({articleId: articleId, comment: comment}))
-    setComment('')
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      postCommentForArticleId({ articleId: articleId, comment: comment })
+    );
+    setComment('');
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label for='comment'>New Comment</label>
-      <input id='comment' value={comment} onChange={e => setComment(e.currentTarget.value)} type='text'/>
-      <button disabled={createPending}>Publish</button>
+      <label for='comment' className='label'>
+        Add Comment:
+      </label>
+      <div id='input-container'>
+        <input
+          id='comment'
+          value={comment}
+          onChange={(e) => setComment(e.currentTarget.value)}
+          type='text'
+        />
+        <button
+          disabled={createPending || comment.length === 0}
+          className='comment-button'
+        >
+          Submit
+        </button>
+      </div>
     </form>
   );
 }
