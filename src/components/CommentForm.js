d0e466a82ca847ddcafc7f95369d061aac3a6262
John Pham <john@highlight.run>
2021-06-21 18:52:41 -0700

    Split CommentForm into smart/dumb components

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,42 +1,58 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   postCommentForArticleId,
   createCommentIsPending,
-} from '../features/comments/commentsSlice';
+} from "../features/comments/commentsSlice";
 
 export default function CommentForm({ articleId }) {
   const dispatch = useDispatch();
-  const [comment, setComment] = useState('');
   const createPending = useSelector(createCommentIsPending);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (comment) => {
     dispatch(
       postCommentForArticleId({ articleId: articleId, comment: comment })
     );
-    setComment('');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label for='comment' className='label'>
+    <CommentFormComponent
+      handleSubmit={handleSubmit}
+      createPending={createPending}
+    />
+  );
+}
+
+export const CommentFormComponent = ({ handleSubmit, createPending }) => {
+  const [comment, setComment] = useState("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit(comment);
+        setComment("");
+      }}
+    >
+      <label htmlFor="comment" className="label">
         Add Comment:
       </label>
-      <div id='input-container'>
+      <div id="input-container">
         <input
-          id='comment'
+          id="comment"
           value={comment}
-          onChange={(e) => setComment(e.currentTarget.value)}
-          type='text'
+          onChange={(e) => {
+            setComment(e.currentTarget.value);
+          }}
+          type="text"
         />
         <button
           disabled={createPending || comment.length === 0}
-          className='comment-button'
+          className="comment-button"
         >
           Submit
         </button>
       </div>
     </form>
   );
-}
+};
